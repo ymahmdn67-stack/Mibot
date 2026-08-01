c100ce7ce9b07dda6ee318dde407f650843a24fe
ymahmdn67-stack <ymahmdn67@gmail.com>
2026-08-01 23:10:18 +0300

    Update St.py

--- St.py
+++ St.py
@@ -294,7 +294,7 @@ async def process_ST_1_charge(card_line: str, proxy_url: str = None) -> tuple[st
 # ==========================================
 async def main():
     # بطاقة اختبار قياسية لـ Stripe
-    test_card = "4211566115568609|12|28|321"
+    test_card = "4211566115568612|12|28|321"
     proxy = None  # يمكنك وضع البروكسي هنا إذا لزم الأمر
 
     print("🚀 جاري اختبار البوابة (ST Charge)...")
